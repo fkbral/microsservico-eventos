6406ab727017a104141b5346c12f970804e300c1
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-16 18:03:17 -0300

    adicionando comentarios

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,4 +1,7 @@
 export class Event {
+  // quando tiver "?" na frente do atributo, significa que ele
+  // é opcional 
+  // se não tiver o "?" na frente, significa que ele é obrigatório
   id?: number;
   title: string = "";
   date: Date = new Date();
