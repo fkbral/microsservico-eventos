d7ee81acd404ee2d723db9b7124a8492223289f7
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-20 18:00:28 -0300

    criando schemas sql

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -1,11 +1,11 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
-const initializeDatabase = async () => {
+const initializeDatase = async () => {
   return open({
     filename: "src/db/database.sqlite",
     driver: sqlite3.Database,
   });
 };
 
-export default initializeDatabase;
+export default initializeDatase;
--- src/db/migrations/001-initial-schema.sql
+++ src/db/migrations/001-initial-schema.sql
@@ -3,14 +3,15 @@ CREATE TABLE IF NOT EXISTS events (
     title TEXT NOT NULL,
     date TEXT NOT NULL,
     time TEXT NOT NULL,
-    location TEXT,
-    description TEXT
+    location TEXT NOT NULL,
+    description TEXT NOT NULL,
 );
 
 -- Tabela de Associação Evento-Convidado (Relação entre eventos e IDs de usuários/convidados)
-CREATE TABLE IF NOT EXISTS event_guests (
+
+CREATE TABLE IF NOT EXISTS events_guests (
     event_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    FOREIGN KEY(event_id) REFERENCES events(id),
+    FOREIGN KEY(event_id) REFERENCES events(id)
     PRIMARY KEY(event_id, user_id)
 );
