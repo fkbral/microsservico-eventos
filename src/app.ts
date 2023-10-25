15627eeb9282633a2d925a59b594d44c19c6088f
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-25 16:35:04 -0300

    bloqueando chamadas que nao sejam do getaway

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 // Importação dos módulos necessários
 import express from "express"; // Framework web para criar a aplicação
 import dotenv from "dotenv"; // Módulo para carregar variáveis de ambiente
-import cors from "cors"; // Middleware para habilitar CORS
+import cors, { CorsOptions } from "cors"; // Middleware para habilitar CORS
+
 import eventRoutes from "./routes/eventRoutes"; // Rotas para os eventos
 
 // Carrega as variáveis de ambiente do arquivo .env
@@ -9,17 +10,6 @@ dotenv.config();
 
 // Cria uma nova instância do express
 const app = express();
-
-// Utiliza o middleware CORS para permitir requisições de diferentes origens
-app.use(
-  cors({
-    origin: ["http://localhost:4000"],
-  })
-);
-
-app.use((req, res) => {
-  res.status(404);
-});
 
 // Habilita o express para tratar requisições com corpo no formato urlencoded
 app.use(express.urlencoded({ extended: false }));
@@ -29,6 +19,29 @@ app.use(express.json());
 
 // Define a porta padrão ou a porta definida no arquivo .env
 const PORT = process.env.PORT || 3000;
+
+const ALLOWED_ORIGIN = "http://localhost:4000";
+
+const corsOptions: CorsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin) {
+      // Se não há cabeçalho de origem, rejeitamos a requisição
+      callback(new Error("Origin header missing or undefined"));
+      return;
+    }
+
+    if (origin === ALLOWED_ORIGIN) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 // Usa as rotas definidas em eventRoutes para todas as requisições que começam com "/events"
 app.use("/events", eventRoutes);
